924aa57dffdbd23679c4ea9c7198a5af59727128
Hansol <hansol4412@naver.com>
2020-10-07 11:23:30 +0900

    chap11

--- sp5-chap11/src/main/java/config/ControllerConfig.java
+++ sp5-chap11/src/main/java/config/ControllerConfig.java
@@ -1,13 +1,18 @@
 package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import controller.RegisterController;
-
+import spring.MemberRegisterService;
 @Configuration
 public class ControllerConfig {
+	@Autowired
+	private MemberRegisterService memberRegSvc;
+	
 	@Bean
 	public RegisterController registerController() {
-		return new RegisterController();
+	RegisterController controller = new RegisterController();
+	controller.setMemberRegisterService(memberRegSvc);
+	return controller;
 	}
 }
