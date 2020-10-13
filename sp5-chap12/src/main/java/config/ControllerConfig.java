1751ab78cd616fcbef03f7719122092a80f1e700
Hansol <hansol4412@naver.com>
2020-10-13 19:25:26 +0900

    chap12

---
+++ sp5-chap12/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,24 @@
+package config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import controller.RegisterController;
+import spring.MemberRegisterService;
+import survey.SurveyController;
+@Configuration
+public class ControllerConfig {
+	@Autowired
+	private MemberRegisterService memberRegSvc;
+	
+	@Bean
+	public RegisterController registerController() {
+	RegisterController controller = new RegisterController();
+	controller.setMemberRegisterService(memberRegSvc);
+	return controller;
+	}
+	
+	@Bean
+	public SurveyController surveyController() {
+		return new SurveyController();
+	}
+}
---
+++ sp5-chap12/src/main/java/spring/Member.java
@@ -0,0 +1,47 @@
+package spring;
+import java.time.LocalDateTime;
+public class Member {
+	private long id;
+	private String email;
+	private String password;
+	private String name;
+	private LocalDateTime regiserDateTime;
+	
+	public Member(String email, String password, String name, LocalDateTime regiserDateTime) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.regiserDateTime =  regiserDateTime;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public LocalDateTime getRegiserDateTime() {
+		return regiserDateTime;
+	}
+
+	public void changePassword(String oldPassword, String newPassword) {
+		if(!password.equals(oldPassword)) throw new WrongIdPasswordException();
+		
+		this.password = newPassword;
+	}
+	
+}
