34b3d5ebba289c92111c5328683626fd90621b0b
Hansol <hansol4412@naver.com>
2020-10-19 17:07:52 +0900

    chap12-1

---
+++ sp5-chap12-1/src/main/java/config/MvcConfig.java
@@ -0,0 +1,49 @@
+package config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.Validator;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	/*
+	 // Bean Validation 사용해서 validator 작성 필요 없음
+	 // controller.RegisterRequestValidator 삭제
+	@Override
+	public Validator getValidator() {
+		return new RegisterRequestValidator();
+	}
+	*/
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("main");
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+		ms.setBasenames("message.label");
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
+	}
+
+}
